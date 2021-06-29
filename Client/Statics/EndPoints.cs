8f4f8abca95687b1f35745ac99c6f001b80b7a1f
Reza Fathpour <Reza.Fathpour@sa.gov.au>
2021-06-29 16:53:42 +0930

    implemented some of the controls

---
+++ Client/Statics/EndPoints.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Client.Statics
+{
+    public static class Endpoints
+    {
+        public static string Prefix = "api"; 
+        public static string MakesEndpoint = $"{Prefix}/makes";
+        public static string ModelsEndpoint = $"{Prefix}/models";
+        public static string ColorsEndpoint = $"{Prefix}/colors";
+        public static string CustomersEndpoint = $"{Prefix}/customers";
+        public static string BookingsEndpoint = $"{Prefix}/bookings";
+        public static string VehiclesEndpoint = $"{Prefix}/vehicles";
+    }
+}
